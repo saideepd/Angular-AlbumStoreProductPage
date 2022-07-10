be68bd7f9b9a7a630eacb8b8453b8543473e3566
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-07-10 19:10:08 +0530

    Added Type assertion to return data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
-import 'rxjs/add/operator/map';
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import { Album } from "./album";
 
 @Injectable()
 export class ProductService {
-  private _albumUrl: string = '../assets/album.json';
+  private _albumUrl: string = "../assets/album.json";
   constructor(private _http: Http) {}
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http
+      .get(this._albumUrl)
+      .map((response) => <Album>response.json());
   }
 }
